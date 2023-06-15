68a8d223c7a672385073313ef6e0504359fd9402
yyxqqq777 <yunxuan517@gmail.com>
2023-06-15 15:17:09 -0400

    Initial Commit

---
+++ aeo/aeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  aeoApp.swift
+//  aeo
+//
+//  Created by Yunxuan Yu on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct aeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
